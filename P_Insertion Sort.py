35e52e56ccf93f6ad2dd34fd34a4ad7d2ce60fa3
Tushar Tyagi <tushar.tyagi.dxb@gmail.com>
2022-01-02 22:34:58 +0400

    Update P_Insertion Sort.py

--- P_Insertion Sort.py
+++ P_Insertion Sort.py
@@ -1,1 +1,11 @@
+#This python program performs insertion sort on any list provided as a parameter to the function
 
+def Insertionsort(lst):
+  for i in range(1,len(lst)):
+    value=lst[i]
+    j=i-1
+    while j>=0 and value<lst[j]:
+      lst[j+1]=lst[j]
+      j=j-1
+    lst[j+1]=value
+  print(lst)
